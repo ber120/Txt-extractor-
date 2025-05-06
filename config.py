e07f3aeb7c51da8195c1ccd1de512e865892419e
ber120 <babluverma335051@gmail.com>
2025-05-06 20:41:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+api_id =546431313
+api_hash = ""
+bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
+auth_users = "46513214"
+sudo_users = [int(num) for num in auth_users.split(",")]
+osowner_users = "56416564654"
+owner_users = [int(num) for num in osowner_users.split(",")]
